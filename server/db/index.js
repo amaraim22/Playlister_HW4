97fc97aad0e9b7b2fa403ea2ead4300be9c01e28
Amara Im <amaraim@icloud.com>
2023-01-08 18:57:31 -0500

    heroku changes

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config();
 
+// mongoose
+//     .connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+//     .catch(e => {
+//         console.error('Connection error', e.message)
+//     })
+
 mongoose
-    .connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+    .connect("mongodb+srv://amaraim22:Holland@2013@playlists.xaiclwh.mongodb.net/playlists?retryWrites=true&w=majority", 
+        { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
